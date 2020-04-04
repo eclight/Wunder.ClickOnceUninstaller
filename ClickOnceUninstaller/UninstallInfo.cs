ca0e0f7c56c21b718dbebba0d9b73440c54d5409
eclight <eclight@users.noreply.github.com>
2020-04-04 19:35:40 +0300

    Add possibility to get a list of installed apps

--- ClickOnceUninstaller/UninstallInfo.cs
+++ ClickOnceUninstaller/UninstallInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 using Microsoft.Win32;
 
 namespace Wunder.ClickOnceUninstaller
@@ -14,32 +15,36 @@ namespace Wunder.ClickOnceUninstaller
 
         public static UninstallInfo Find(string appName)
         {
+            return GetInstalledApps().FirstOrDefault(x => x.DisplayName == appName);
+        }
+
+        public static IEnumerable<UninstallInfo> GetInstalledApps()
+        {
             var uninstall = Registry.CurrentUser.OpenSubKey(UninstallRegistryPath);
             if (uninstall != null)
             {
                 foreach (var app in uninstall.GetSubKeyNames())
                 {
                     var sub = uninstall.OpenSubKey(app);
-                    if (sub != null && sub.GetValue("DisplayName") as string == appName)
+
+                    yield return new UninstallInfo
                     {
-                        return new UninstallInfo
-                                   {
-                                       Key = app,
-                                       UninstallString = sub.GetValue("UninstallString") as string,
-                                       ShortcutFolderName = sub.GetValue("ShortcutFolderName") as string,
-                                       ShortcutSuiteName = sub.GetValue("ShortcutSuiteName") as string,
-                                       ShortcutFileName = sub.GetValue("ShortcutFileName") as string,
-                                       SupportShortcutFileName = sub.GetValue("SupportShortcutFileName") as string,
-                                       Version = sub.GetValue("DisplayVersion") as string
-                                   };
-                    }
+                        Key = app,
+                        DisplayName = sub.GetValue("DisplayName") as string,
+                        UninstallString = sub.GetValue("UninstallString") as string,
+                        ShortcutFolderName = sub.GetValue("ShortcutFolderName") as string,
+                        ShortcutSuiteName = sub.GetValue("ShortcutSuiteName") as string,
+                        ShortcutFileName = sub.GetValue("ShortcutFileName") as string,
+                        SupportShortcutFileName = sub.GetValue("SupportShortcutFileName") as string,
+                        Version = sub.GetValue("DisplayVersion") as string
+                    };
                 }
             }
-
-            return null;
         }
 
         public string Key { get; set; }
+
+        public string DisplayName { get; private set; }
 
         public string UninstallString { get; private set; }
 
